62589ec5fd8b23ba8aea42ec0ac603af868f1e95
UnknownKotKot <pod556@list.ru>
2021-10-08 15:07:53 +0300

    HW_6 http://localhost:3000/

--- front-service/src/main/resources/static/index.js
+++ front-service/src/main/resources/static/index.js
@@ -1,10 +1,10 @@
 angular.module('market-app', []).controller('indexController', function ($scope, $http) {
     console.log('web app started');
 
-    const contextPath = 'http://localhost:8186/market/';
+    const contextPath = 'http://localhost:5555/product-service/market/api/v1/products';
 
     $scope.loadProducts = function () {
-        $http.get(contextPath + 'test_dto')
+        $http.get(contextPath)
             .then(function (response) {
                 console.log(response)
                 $scope.products = response.data;
--- product-service/src/main/java/com/cloud/demo/product/api/ProductRepository.java
+++ product-service/src/main/java/com/cloud/demo/product/api/ProductRepository.java
@@ -4,13 +4,6 @@ import com.cloud.demo.product.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-import java.util.Optional;
-
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
-    List<Product> findAllByPriceGreaterThanEqual(int minPrice);
-    List<Product> findAllByPriceLessThanEqual(int maxPrice);
-    List<Product> findAllByPriceBetween(int minPrice, int maxPrice);
-    Optional<Product> findByTitle(String title);
 }
